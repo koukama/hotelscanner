d2acbbe206568fab114f0bb9e0d282c4041e2cc3
Asmae AMZANI <amzani.asmae@gmail.com>
2019-05-30 23:42:23 +0200

    bugfix: Api

--- BookingAPI/Startup.cs
+++ BookingAPI/Startup.cs
@@ -66,7 +66,7 @@ namespace BookingAPI
             }
 
             
-            app.UseCors("Default");
+            app.UseMvc();
             app.UseCorsMiddleware();
 
             bc.CreateSeedData();
